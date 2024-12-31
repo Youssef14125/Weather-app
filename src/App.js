367df15499c887f1bc1863d3e9c753b61b6c90e3
Youssef14125 <snk471@gmail.com>
2024-12-31 05:40:22 +0200

    First v.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
