b100a04aa16347919e848223b1a69f035539d60e
Youssef14125 <snk471@gmail.com>
2024-12-31 05:51:35 +0200

    Quick fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class Counter extends React.Component {
 
     this.state = { count: 5 };
     this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
   }
 
   handleDecrement() {
@@ -13,12 +14,21 @@ class Counter extends React.Component {
       return { count: curState.count - 1 };
     });
   }
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
+  }
   render() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + this.state.count);
     return (
       <div>
         <button onClick={this.handleDecrement}>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <span>
+          {date.toDateString()}({this.state.count})
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
